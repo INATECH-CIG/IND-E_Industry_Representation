6f713e8881678e7b39373a2c2c254ddffa16331d
Nick Harder <nick.harder94@gmail.com>
2022-04-19 16:27:13 +0200

    v0.3 -working cost minimization with electricity consumption rules -missing gas and coal rules

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ Created on Mon Apr  4 13:07:25 2022
 @author: louis
 """
 # %%
-import os as os
-#os.chdir('C:/Users/louis/UNI_SSE/Research/IND-E')
-
-
 import pandas as pd
-
 import pyomo.environ as pyo
-
 from pyomo.opt import SolverFactory
 # %%
 solver = pyo.SolverFactory('glpk')  # alternative : gurobi 
 
-filename = os.path.join(os.getcwd(), "Data_.csv")
-raw_data = pd.read_csv(filename, sep = ';', index_col=0)
+input_data = pd.read_csv('input/Data.csv', sep = ';', index_col=0)
 
-price_elec = raw_data['electricity_price']  #could also use raw_data['electricity_price].iloc[i] below
+price_elec = input_data['electricity_price']  #could also use raw_data['electricity_price].iloc[i] below
 price_ng = None
 price_coal = None
 
 # Dictionaries for Specific ELec and Mass Balance Ratio Coefficients 
-specific_elec_consumption = {1:.37, 2:.127, 3:.575}
-ratio = {1:1.66, 2:1.03, 3:1}
+spec_elec_cons = {'electric_heater': .37,
+                             'iron_reduction': .127,
+                             'arc_furnace': .575}
+
+iron_mass_ratio = {'iron': 1.66,
+         'DRI': 1.03,
+         'liquid_steel': 1}
+
+optimization_horizon = 5
 
 # =============================================================================
 # Segment Problem to first find optimized electricity consumption
@@ -38,49 +38,78 @@ ratio = {1:1.66, 2:1.03, 3:1}
 #                        - solution for lowest cost
 # =============================================================================
 # %%
-
-def Electricity_Opt(specific_elec_consumption, ratio):
-    
+def Electricity_Opt(spec_elec_cons, iron_mass_ratio, steel_prod, optimization_horizon):
     model = pyo.ConcreteModel()
     
-    model.i = pyo.RangeSet(1, len(specific_elec_consumption.keys()))
+    model.t = pyo.RangeSet(1, optimization_horizon)
+        
+    model.iron_ore = pyo.Var(model.t, domain = pyo.NonNegativeReals, bounds = (62.5,250))
+    model.dri = pyo.Var(model.t, domain = pyo.NonNegativeReals, bounds = (40,155))
+    model.liquid_steel = pyo.Var(model.t, domain = pyo.NonNegativeReals, bounds = (37.5,150))
+
+    model.elec_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
+    #model.gas_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
+    #model.coal_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
+
+    model.elec_cost = pyo.Var(model.t)
+
+    #represents the step of electric arc furnace
+    def eaf_rule(model, t):
+        return model.liquid_steel[t] == model.dri[t] / iron_mass_ratio['DRI']
+
+    #represents the direct reduction plant
+    def iron_reduction_rule(model, t):
+        return model.dri[t] == model.iron_ore[t] / iron_mass_ratio['iron']
+
+    #total electricity consumption
+    def elec_consumption_rule(model, t):
+        return model.elec_cons[t] == spec_elec_cons['electric_heater']*model.iron_ore[t] + \
+            spec_elec_cons['iron_reduction']*model.dri[t] + \
+            spec_elec_cons['arc_furnace']*model.liquid_steel[t]
+
+    #total electricity cost
+    def elec_cost_rule(model, t):
+        return model.elec_cost[t] == input_data['electricity_price'].iat[t]*model.elec_cons[t]
+
+    def total_steel_prod_rule(model):
+        return pyo.quicksum(model.liquid_steel[t] for t in model.t) >= steel_prod
+
+    #cost objective function
+    def cost_obj_rule(model):
+        return pyo.quicksum(model.elec_cost[t] for t in model.t)
+
+    model.eaf_rule = pyo.Constraint(model.t, rule=eaf_rule)
+    model.iron_reduction_rule = pyo.Constraint(model.t, rule=iron_reduction_rule)
+    model.elec_consumption_rule = pyo.Constraint(model.t, rule=elec_consumption_rule)
+    model.elec_cost_rule = pyo.Constraint(model.t, rule=elec_cost_rule)
+    model.total_steel_prod_rule = pyo.Constraint(rule = total_steel_prod_rule)
     
-    model.C = pyo.Param(model.i, initialize = specific_elec_consumption)
-    model.F = pyo.Param(model.i, initialize = ratio)
-    
-    
-    model.I = pyo.Var(domain = pyo.NonNegativeReals, bounds = (62.5,250))       
-    model.DRI = pyo.Var(domain = pyo.NonNegativeReals, bounds = (40,155))
-    model.LS = pyo.Var(domain = pyo.NonNegativeReals, bounds = (37.5,150))
-    
-    def obj_rule(model):
-        return model.C[1]*model.I + model.C[2]*model.DRI + model.C[3]*model.LS
-    
-    model.obj = pyo.Objective(rule = obj_rule, sense = pyo.minimize)
-    
-    
-    def con_rule1(model):
-        return model.F[1]*model.I == model.F[2]* model.DRI
-    
-    model.con1 = pyo.Constraint(rule = con_rule1)
-    
-    def con_rule2(model):
-        return model.F[2]*model.DRI == model.F[3]*model.LS
-    
-    model.con2 = pyo.Constraint(rule = con_rule2)
-    
-    solver.solve(model)
-    
+    model.obj = pyo.Objective(rule = cost_obj_rule, sense = pyo.minimize)
+
     return model
 # %%
-model = Electricity_Opt(specific_elec_consumption, ratio)
+steel_prod = 500
+model = Electricity_Opt(spec_elec_cons, iron_mass_ratio, steel_prod, optimization_horizon)
+solved_model = solver.solve(model)
+# %%
+def get_values(model):
+    iron_ore = []
+    dri = []
+    liquid_steel = []
+    elec_cons = []
+    elec_cost = []
+    
+    for i in range(1,optimization_horizon+1):
+        iron_ore.append(model.iron_ore[i].value)
+        dri.append(model.dri[i].value)
+        liquid_steel.append(model.liquid_steel[i].value)
+        elec_cons.append(model.elec_cons[i].value)
+        elec_cost.append(model.elec_cost[i].value)
 
-print(pyo.value(model.obj))
-print(pyo.value(model.I))
-print(pyo.value(model.DRI))
-print(pyo.value(model.LS))
+    return iron_ore, dri, liquid_steel, elec_cons, elec_cost
 
-
+# %%
+iron_ore, dri, liquid_steel, elec_cons, elec_cost = get_values(model)
 
 
 
