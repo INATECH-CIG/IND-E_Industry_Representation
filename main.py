b9c28295f14c308df4ec8057cf956e9e81c59dee
lrserpe <102610230+lrserpe@users.noreply.github.com>
2022-04-20 18:01:05 -0600

    v.04
    Added NG and Coal consumption and cost

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ Created on Mon Apr  4 13:07:25 2022
 import pandas as pd
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
+import pprint
 # %%
 solver = pyo.SolverFactory('glpk')  # alternative : gurobi 
 
 input_data = pd.read_csv('input/Data.csv', sep = ';', index_col=0)
 
+fuel_data = pd.read_csv('input/Fuel.csv', sep = ',', index_col=(0))
+
 price_elec = input_data['electricity_price']  #could also use raw_data['electricity_price].iloc[i] below
-price_ng = None
-price_coal = None
+price_ng = fuel_data['natural gas']
+price_coal = fuel_data['hard coal']
 
 # Dictionaries for Specific ELec and Mass Balance Ratio Coefficients 
 spec_elec_cons = {'electric_heater': .37,
                              'iron_reduction': .127,
                              'arc_furnace': .575}
+spec_ng_cons = {'iron_reduction': 1.56,
+                'arc_furnace' : .216}
+
+spec_coal_cons = {'arc_furnace' : .028}
 
 iron_mass_ratio = {'iron': 1.66,
          'DRI': 1.03,
@@ -28,17 +35,9 @@ iron_mass_ratio = {'iron': 1.66,
 
 optimization_horizon = 5
 
-# =============================================================================
-# Segment Problem to first find optimized electricity consumption
-# 
-#         - use the solved values for Iron, DRI, and LS to determine NG and Coal demand
-#                  -solution for min elec power 
-#         - add time step to second optimization problem to include pricing
-#                  -need NG and Coal prcies to determine optimized Elec, NG, Coal mixture 
-#                        - solution for lowest cost
-# =============================================================================
+
 # %%
-def Electricity_Opt(spec_elec_cons, iron_mass_ratio, steel_prod, optimization_horizon):
+def Price_Opt(spec_elec_cons, spec_ng_cons, spec_coal_cons, iron_mass_ratio, steel_prod, optimization_horizon):
     model = pyo.ConcreteModel()
     
     model.t = pyo.RangeSet(1, optimization_horizon)
@@ -48,10 +47,12 @@ def Electricity_Opt(spec_elec_cons, iron_mass_ratio, steel_prod, optimization_ho
     model.liquid_steel = pyo.Var(model.t, domain = pyo.NonNegativeReals, bounds = (37.5,150))
 
     model.elec_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
-    #model.gas_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
-    #model.coal_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
+    model.ng_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
+    model.coal_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
 
     model.elec_cost = pyo.Var(model.t)
+    model.ng_cost = pyo.Var(model.t)
+    model.coal_cost = pyo.Var(model.t)
 
     #represents the step of electric arc furnace
     def eaf_rule(model, t):
@@ -70,18 +71,39 @@ def Electricity_Opt(spec_elec_cons, iron_mass_ratio, steel_prod, optimization_ho
     #total electricity cost
     def elec_cost_rule(model, t):
         return model.elec_cost[t] == input_data['electricity_price'].iat[t]*model.elec_cons[t]
+    
+    #total NG consumption
+    def ng_consumption_rule(model,t):
+        return model.ng_cons[t] == spec_ng_cons['iron_reduction']*model.dri[t] + \
+            spec_ng_cons['arc_furnace']*model.liquid_steel[t] 
+    
+    #total NG cost
+    def ng_cost_rule(model,t):
+        return model.ng_cost[t] == fuel_data['natural gas'].iat[t]*model.ng_cons[t]
+    
+    #total coal consumption
+    def coal_consumption_rule(model, t):
+        return model.coal_cons[t] == spec_coal_cons['arc_furnace']*model.liquid_steel[t]
+    
+    #total coal cost
+    def coal_cost_rule(model,t):
+        return model.coal_cost[t] == fuel_data['hard coal'].iat[t]*model.coal_cons[t]
 
     def total_steel_prod_rule(model):
         return pyo.quicksum(model.liquid_steel[t] for t in model.t) >= steel_prod
 
     #cost objective function
     def cost_obj_rule(model):
-        return pyo.quicksum(model.elec_cost[t] for t in model.t)
+        return pyo.quicksum(model.elec_cost[t] + model.ng_cost[t] + model.coal_cost[t] for t in model.t)
 
     model.eaf_rule = pyo.Constraint(model.t, rule=eaf_rule)
     model.iron_reduction_rule = pyo.Constraint(model.t, rule=iron_reduction_rule)
     model.elec_consumption_rule = pyo.Constraint(model.t, rule=elec_consumption_rule)
     model.elec_cost_rule = pyo.Constraint(model.t, rule=elec_cost_rule)
+    model.ng_consumption_rule = pyo.Constraint(model.t, rule = ng_consumption_rule)
+    model.ng_cost_rule = pyo.Constraint(model.t, rule = ng_cost_rule)
+    model.coal_consumption_rule = pyo.Constraint(model.t, rule = coal_consumption_rule)
+    model.coal_cost_rule = pyo.Constraint(model.t, rule = coal_cost_rule)
     model.total_steel_prod_rule = pyo.Constraint(rule = total_steel_prod_rule)
     
     model.obj = pyo.Objective(rule = cost_obj_rule, sense = pyo.minimize)
@@ -89,7 +111,7 @@ def Electricity_Opt(spec_elec_cons, iron_mass_ratio, steel_prod, optimization_ho
     return model
 # %%
 steel_prod = 500
-model = Electricity_Opt(spec_elec_cons, iron_mass_ratio, steel_prod, optimization_horizon)
+model = Price_Opt(spec_elec_cons, spec_ng_cons, spec_coal_cons, iron_mass_ratio, steel_prod, optimization_horizon)
 solved_model = solver.solve(model)
 # %%
 def get_values(model):
@@ -98,6 +120,11 @@ def get_values(model):
     liquid_steel = []
     elec_cons = []
     elec_cost = []
+    ng_cons = []
+    ng_cost = []
+    coal_cons = []
+    coal_cost = []
+    
     
     for i in range(1,optimization_horizon+1):
         iron_ore.append(model.iron_ore[i].value)
@@ -105,11 +132,15 @@ def get_values(model):
         liquid_steel.append(model.liquid_steel[i].value)
         elec_cons.append(model.elec_cons[i].value)
         elec_cost.append(model.elec_cost[i].value)
+        ng_cons.append(model.ng_cons[i].value)
+        ng_cost.append(model.ng_cost[i].value)
+        coal_cons.append(model.coal_cons[i].value)
+        coal_cost.append(model.coal_cost[i].value)
 
-    return iron_ore, dri, liquid_steel, elec_cons, elec_cost
+    return iron_ore, dri, liquid_steel, elec_cons, elec_cost, ng_cons, ng_cost, coal_cons, coal_cost
 
 # %%
-iron_ore, dri, liquid_steel, elec_cons, elec_cost = get_values(model)
+iron_ore, dri, liquid_steel, elec_cons, elec_cost, ng_cons, ng_cost, coal_cons, coal_cost = get_values(model)
 
 
 
