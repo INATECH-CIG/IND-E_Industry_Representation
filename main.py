bffdfb75ff117310dc26b4e1ade949e9aaa5f051
lrserpe <102610230+lrserpe@users.noreply.github.com>
2022-05-19 15:50:30 +0200

    v0.10
    -moved functions to separate files
    - re-structured

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Thu May 19 14:48:30 2022
+
+@author: louis
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Sun May  8 07:58:23 2022
 
 @author: louis
@@ -17,6 +24,7 @@ import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
 import pprint
 from misc import time_series_plot
+from optimization import Price_Opt
 
 import matplotlib.pyplot as plt
 # %%
@@ -52,183 +60,60 @@ power_limits = {'EH_max': 93,
                 'Total_max': 198,
                 'Total_min':49 }
 
-optimization_horizon = 24
-
-#Set x-axis for line series plot function
-
-
-# %%
-def Price_Opt(spec_elec_cons, spec_ng_cons, spec_coal_cons, iron_mass_ratio,
-              steel_prod, optimization_horizon, flexibility_params):
-   
-    model = pyo.ConcreteModel()
-    
-    model.t = pyo.RangeSet(1, optimization_horizon)
-            
-    model.iron_ore = pyo.Var(model.t, domain = pyo.NonNegativeReals, bounds = (62.5,250)) #bounds 62.5- 250
-    model.dri = pyo.Var(model.t, domain = pyo.NonNegativeReals, bounds = (40,155))        #bounds 40-155
-    model.liquid_steel = pyo.Var(model.t, domain = pyo.NonNegativeReals, bounds = (37.5,150))  #bounds 37.5 - 150
-
-    model.elec_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
-    model.ng_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
-    model.coal_cons = pyo.Var(model.t, domain = pyo.NonNegativeReals)
-
-    model.elec_cost = pyo.Var(model.t)
-    model.ng_cost = pyo.Var(model.t)
-    model.coal_cost = pyo.Var(model.t)
-    
-    #flexibility variables 
-    model.pos_flex = pyo.Var(model.t,domain = pyo.NonNegativeReals )  #could set bounds to power limits
-    model.neg_flex = pyo.Var(model.t, domain = pyo.NonNegativeReals)
-
-    #represents the step of electric arc furnace
-    def eaf_rule(model, t):
-        return model.liquid_steel[t] == model.dri[t] / iron_mass_ratio['DRI']
-
-    #represents the direct reduction plant
-    def iron_reduction_rule(model, t):
-        return model.dri[t] == model.iron_ore[t] / iron_mass_ratio['iron']
-
-    #total electricity consumption
-    def elec_consumption_rule(model, t):
-        return model.elec_cons[t] == spec_elec_cons['electric_heater']*model.iron_ore[t] + \
-            spec_elec_cons['iron_reduction']*model.dri[t] + \
-            spec_elec_cons['arc_furnace']*model.liquid_steel[t]
-
-    #total electricity cost
-    def elec_cost_rule(model, t):
-        return model.elec_cost[t] == input_data['electricity_price'].iat[t]*model.elec_cons[t]
-    
-# =============================================================================
-#     #flexibility rule
-    def elec_flex_rule(model, t):
-        if t == flexibility_params['hour_called']:
-        #reduce consumption
-            if flexibility_params['type'] == 'pos':
-                return model.elec_cons[t] <= flexibility_params['amt_called']
-            else:
-                return model.elec_cons[t] >= flexibility_params['amt_called']
-        else:
-            return model.elec_cons[t] >= 0
-#         
-# =============================================================================
-        
-    #total NG consumption
-    def ng_consumption_rule(model,t):
-        return model.ng_cons[t] == spec_ng_cons['iron_reduction']*model.dri[t] + \
-            spec_ng_cons['arc_furnace']*model.liquid_steel[t] 
-    
-    #total NG cost
-    def ng_cost_rule(model,t):
-        return model.ng_cost[t] == fuel_data['natural gas'].iat[t]*model.ng_cons[t]
-    
-    #total coal consumption
-    def coal_consumption_rule(model, t):
-        return model.coal_cons[t] == spec_coal_cons['arc_furnace']*model.liquid_steel[t]
-    
-    #total coal cost
-    def coal_cost_rule(model,t):
-        return model.coal_cost[t] == fuel_data['hard coal'].iat[t]*model.coal_cons[t]
-
-    def total_steel_prod_rule(model):
-        return pyo.quicksum(model.liquid_steel[t] for t in model.t) >= steel_prod
-
-    #cost objective function (elec, NG, and coal)
-    def cost_obj_rule(model):
-        return pyo.quicksum(model.elec_cost[t] + model.ng_cost[t] + model.coal_cost[t] for t in model.t)
-
-    model.eaf_rule = pyo.Constraint(model.t, rule=eaf_rule)
-    model.iron_reduction_rule = pyo.Constraint(model.t, rule=iron_reduction_rule)
-    model.elec_consumption_rule = pyo.Constraint(model.t, rule=elec_consumption_rule)
-    model.elec_cost_rule = pyo.Constraint(model.t, rule=elec_cost_rule)
-    model.ng_consumption_rule = pyo.Constraint(model.t, rule = ng_consumption_rule)
-    model.ng_cost_rule = pyo.Constraint(model.t, rule = ng_cost_rule)
-    model.coal_consumption_rule = pyo.Constraint(model.t, rule = coal_consumption_rule)
-    model.coal_cost_rule = pyo.Constraint(model.t, rule = coal_cost_rule)
-    model.total_steel_prod_rule = pyo.Constraint(rule = total_steel_prod_rule)
-    
-    #flexibility constraint included if called
-    if flexibility_params is not None:
-        model.elec_flex_rule = pyo.Constraint(model.t, rule = elec_flex_rule)
-        
-    model.obj = pyo.Objective(rule = cost_obj_rule, sense = pyo.minimize)
-
-    return model
-# %%
-#Dictionary for flexibility parameters
-flexibility = {'hour_called': 10,
-               'amt_called': pos_flex_total[9],
-               'type': 'pos'}
+#%%
+# Run model without flexibility 
 
 steel_prod = 2000
-model = Price_Opt(spec_elec_cons, spec_ng_cons, spec_coal_cons, iron_mass_ratio, \
+optimization_horizon = 24
+
+model = Price_Opt(input_data, fuel_data, spec_elec_cons, spec_ng_cons, spec_coal_cons, iron_mass_ratio, \
                   steel_prod, optimization_horizon, None)
 solved_model = solver.solve(model)
-# %%
-def get_values(model):
-    
-    time = []
-    elec_price = []
-    iron_ore = []
-    dri = []
-    liquid_steel = []
-    elec_cons = []
-    elec_cost = []
-    ng_cons = []
-    ng_cost = []
-    coal_cons = []
-    coal_cost = []
-    total_energy_cons = []
-    total_fuel_price = []
-    total_energy_cost = []
-    pos_flex = []
-    neg_flex = []
 
-# list to check flexibility calc in model 
-    elec_cons_EH = []
-    elec_cons_DRP = []
-    elec_cons_AF = []
-    
-    
-    
-    for i in range(1,optimization_horizon+1):
-        time.append(i-1 )
-        elec_price.append(input_data['electricity_price'][i])
-        iron_ore.append(model.iron_ore[i].value)
-        dri.append(model.dri[i].value)
-        liquid_steel.append(model.liquid_steel[i].value)
-        elec_cons.append(model.elec_cons[i].value)
-        elec_cost.append(model.elec_cost[i].value)
-        ng_cons.append(model.ng_cons[i].value)
-        ng_cost.append(model.ng_cost[i].value)
-        coal_cons.append(model.coal_cons[i].value)
-        coal_cost.append(model.coal_cost[i].value)
-                
-        elec_cons_EH.append(spec_elec_cons['electric_heater']*model.iron_ore[i].value)
-        elec_cons_DRP.append(spec_elec_cons['iron_reduction']*model.dri[i].value)
-        elec_cons_AF.append( spec_elec_cons['arc_furnace']*model.liquid_steel[i].value)
-        
- # quick model check that consumption increases with decreasing fuel price 
-        total_energy_cons.append(model.elec_cons[i].value + model.ng_cons[i].value + model.coal_cons[i].value) 
-        total_fuel_price.append(input_data['electricity_price'].iat[i] +\
-                                 fuel_data['natural gas'].iat[i] +\
-                                 fuel_data['hard coal'].iat[i])
-        total_energy_cost.append(model.elec_cost[i].value + model.ng_cost[i].value + model.coal_cost[i].value )
 
-        pos_flex.append(model.pos_flex[i].value) 
-        neg_flex.append(model.neg_flex[i].value)
-
-    return time,elec_price,iron_ore, dri, liquid_steel, elec_cons, elec_cost, ng_cons, ng_cost, coal_cons, coal_cost,\
-        total_energy_cons,total_fuel_price, total_energy_cost, elec_cons_EH, elec_cons_DRP, elec_cons_AF, pos_flex, neg_flex
-
-# %%
 time,elec_price, iron_ore, dri, liquid_steel, elec_cons, elec_cost, ng_cons, ng_cost, coal_cons,\
     coal_cost, total_energy_cons,total_fuel_price, total_energy_cost,\
-        elec_cons_EH, elec_cons_DRP, elec_cons_AF, pos_flex, neg_flex = get_values(model)
+        elec_cons_EH, elec_cons_DRP, elec_cons_AF, pos_flex, neg_flex = get_values(model,optimization_horizon,\
+                                                                                   input_data, fuel_data, \
+                                                                                       spec_elec_cons)
 
+time_series_plot(time,elec_cons)
+
+base_case_cons = elec_cons
+base_case_cost = sum(elec_cost)
   
     
 # %%
+# Run model with flexibility called 
+
+flex_hour = 10
+flexibility = {'hour_called': flex_hour,
+               'amt_called': 60,
+               'type': 'pos'}
+
+steel_prod = 2000
+optimization_horizon = 24
+
+model = Price_Opt(input_data, fuel_data, spec_elec_cons, spec_ng_cons, spec_coal_cons, iron_mass_ratio, \
+                  steel_prod, optimization_horizon, flexibility)
+solved_model = solver.solve(model)
+
+
+time,elec_price, iron_ore, dri, liquid_steel, elec_cons, elec_cost, ng_cons, ng_cost, coal_cons,\
+    coal_cost, total_energy_cons,total_fuel_price, total_energy_cost,\
+        elec_cons_EH, elec_cons_DRP, elec_cons_AF, pos_flex, neg_flex = get_values(model,optimization_horizon,\
+                                                                                   input_data, fuel_data, \
+                                                                                       spec_elec_cons)
+
+time_series_plot(time,elec_cons)
+
+flex_case_cons = elec_cons
+flex_case_cost = sum(elec_cost)
+  
+
+cost_flexibility = (flex_case_cost - base_case_cost)/100   #Euro/MWh
+
+#%%
 
 # Available Flexibility at each time step
 
@@ -254,38 +139,3 @@ def flexibility_available(model, elec_cons) :
 
 pos_flex_total, neg_flex_total = flexibility_available(model, elec_cons) 
 
-
-# %%
-plt.figure()
-plt.plot(range(1,optimization_horizon+1), elec_cons)
-plt.xticks(range(1,optimization_horizon+1))
-plt.show()
-
-
-
-# %%
-opt_elec_cons = elec_cons
-opt_cost = sum(elec_cost)
-# %%
-flex_hour = 10
-flexibility = {'hour_called': flex_hour,
-               'amt_called': 60,
-               'type': 'pos'}
-
-steel_prod = 2000
-model = Price_Opt(spec_elec_cons, spec_ng_cons, spec_coal_cons, iron_mass_ratio, \
-                  steel_prod, optimization_horizon, flexibility)
-solved_model = solver.solve(model)
-
-# %%
-time,elec_price, iron_ore, dri, liquid_steel, elec_cons, elec_cost, ng_cons, ng_cost, coal_cons,\
-    coal_cost, total_energy_cons,total_fuel_price, total_energy_cost,\
-        elec_cons_EH, elec_cons_DRP, elec_cons_AF, pos_flex, neg_flex = get_values(model)
-
-# %%
-plt.figure()
-plt.plot(range(1,optimization_horizon+1), elec_cons)
-plt.xticks(range(1,optimization_horizon+1))
-plt.show()
-
-# %%
