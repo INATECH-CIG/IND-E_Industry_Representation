v0.16 -fixed limits calculation -smaller fixes in code -fixed flexibility call -further tests required

main.py
@@ -20,7 +20,6 @@ input_data = pd.read_csv('input/Data.csv', sep = ';', index_col=0)
 fuel_data = pd.read_csv('input/Fuel.csv', sep = ',', index_col=0)
 
 price_elec = np.array(input_data['electricity_price'])  # cents/KWh
-price_elec = price_elec*10    #euro/MWh
 price_ng = np.array(fuel_data['natural gas'])
 price_coal = np.array(fuel_data['hard coal'])
 
@@ -37,64 +36,52 @@ spec_coal_cons = {'arc_furnace' : .028}
 iron_mass_ratio = {'iron': 1.66,
                    'DRI': 1.03,
                    'liquid_steel': 1}
-#%%
+
+plant_cap = 3000
 steel_prod = 2000
 optimization_horizon = 24
-#limits = power_limits(1, spec_elec_cons, iron_mass_ratio, optimization_horizon, steel_prod)
 
-#MW
-limits = {'max_ls': 150,
-            'min_ls': 37.5,
-            'max_dri': 155,
-            'min_dri': 40,
-            'max_iron': 250,
-            'min_iron': 62.5,
-            'EH_max': 93 ,               #93
-            'EH_min': 23,               #23
-            'DRP_max': 20,              #20
-            'DRP_min': 5,               #5
-            'AF_max': 85,               #85
-            'AF_min': 21,               #21
-            'Total_max': 198,           #198
-            'Total_min': 49             #49
-
-            }
- 
+#%%
+limits = power_limits(plant_cap, spec_elec_cons, iron_mass_ratio, optimization_horizon)
 
 #%%
 # Run model without flexibility 
-steel_prod = 2000
-optimization_horizon = 24
+base_model = Price_Opt(input_data=input_data,
+                       fuel_data=fuel_data,
+                       spec_elec_cons=spec_elec_cons,
+                       spec_ng_cons=spec_ng_cons,
+                       spec_coal_cons=spec_coal_cons,
+                       iron_mass_ratio=iron_mass_ratio,
+                       steel_prod=steel_prod,
+                       optimization_horizon=optimization_horizon,
+                       limits=limits,
+                       flexibility_params=None)
 
-model = Price_Opt(input_data=input_data,
-                  fuel_data=fuel_data,
-                  spec_elec_cons=spec_elec_cons,
-                  spec_ng_cons=spec_ng_cons,
-                  spec_coal_cons=spec_coal_cons,
-                  iron_mass_ratio=iron_mass_ratio,
-                  steel_prod=steel_prod,
-                  optimization_horizon=optimization_horizon,
-                  limits = limits,
-                  flexibility_params=None)
+solved_model = solver.solve(base_model)
 
-solved_model = solver.solve(model)
-
-model_params = get_values(model,optimization_horizon, input_data, fuel_data, spec_elec_cons)
+base_model_params = get_values(base_model, optimization_horizon, input_data, fuel_data, spec_elec_cons)
                                                                                   
-time_series_plot(model_params['time_step'],model_params['elec_cons'])
-time_series_plot(model_params['time_step'],model_params['dri_direct'],model_params['dri_to_storage'], model_params['AF_elec_cons'],model_params['storage'])
-time_series_plot(model_params['time_step'],model_params['EH_elec_cons'],model_params['DRP_elec_cons'],model_params['AF_elec_cons'])
-time_series_plot(model_params['time_step'],price_elec[0:optimization_horizon])
+time_series_plot(base_model_params['time_step'],
+                 base_model_params['elec_cons'])
 
-base_case_cons = np.array(model_params['elec_cons'])
+time_series_plot(base_model_params['time_step'], base_model_params['dri_direct'],
+                 base_model_params['dri_to_storage'], base_model_params['AF_elec_cons'], base_model_params['storage'])
+
+time_series_plot(base_model_params['time_step'], base_model_params['EH_elec_cons'],
+                 base_model_params['DRP_elec_cons'], base_model_params['AF_elec_cons'])
+
+time_series_plot(base_model_params['time_step'],
+                 price_elec[0:optimization_horizon])
+
+base_case_cons = np.array(base_model_params['elec_cons'])
 base_case_cost = sum(base_case_cons*price_elec[0:optimization_horizon])
 
  
 #%%
 #Flexibility available at each time step - quanity and cost 
-pos_flex_total, neg_flex_total = flexibility_available(model, model_params['elec_cons'], limits, optimization_horizon) 
+pos_flex_total, neg_flex_total = flexibility_available(base_model, base_model_params['elec_cons'], limits, optimization_horizon) 
     
-time_series_plot(model_params['time_step'],model_params['elec_cons'],neg_flex_total,pos_flex_total)
+time_series_plot(base_model_params['time_step'], base_model_params['elec_cons'], neg_flex_total, pos_flex_total)
 
 pos_flex_hourly = np.array(pos_flex_total)
 neg_flex_hourly = np.array(neg_flex_total)
@@ -108,38 +95,43 @@ neg_flex_cost_hourly = neg_flex_hourly*price_elec[0:optimization_horizon]
 # Run model with flexibility called 
 
 flex_hour = 10
+flex_type = 'pos'
+
+if flex_type == 'pos':
+    flex_amount = pos_flex_hourly[flex_hour]
+    cons_signal = base_model_params['elec_cons'][flex_hour] - flex_amount
+else:
+    flex_amount = neg_flex_hourly[flex_hour]
+    cons_signal = base_model_params['elec_cons'][flex_hour] + flex_amount
+
 flexibility = {'hour_called': flex_hour,
-               'amt_called': 28,
-               'type': 'pos'}
+               'cons_signal': cons_signal,
+               'type': flex_type}
 
-steel_prod = 2000
-optimization_horizon = 24
+flex_model = Price_Opt(input_data=input_data,
+                       fuel_data=fuel_data,
+                       spec_elec_cons=spec_elec_cons,
+                       spec_ng_cons=spec_ng_cons,
+                       spec_coal_cons=spec_coal_cons,
+                       iron_mass_ratio=iron_mass_ratio,
+                       steel_prod=steel_prod,
+                       optimization_horizon=optimization_horizon,
+                       limits=limits,
+                       flexibility_params=flexibility)
 
-model = Price_Opt(input_data=input_data,
-                  fuel_data=fuel_data,
-                  spec_elec_cons=spec_elec_cons,
-                  spec_ng_cons=spec_ng_cons,
-                  spec_coal_cons=spec_coal_cons,
-                  iron_mass_ratio=iron_mass_ratio,
-                  steel_prod=steel_prod,
-                  optimization_horizon=optimization_horizon,
-                  limits = limits,
-                  flexibility_params=flexibility)
+solved_model = solver.solve(flex_model)
 
-solved_model = solver.solve(model)
+flex_model_params = get_values(model=flex_model,
+                               optimization_horizon=optimization_horizon,
+                               input_data=input_data,
+                               fuel_data=fuel_data,
+                               spec_elec_cons=spec_elec_cons)
 
+time_series_plot(flex_model_params['time_step'], flex_model_params['elec_cons'])
 
-model_params = get_values(model=model,
-                          optimization_horizon=optimization_horizon,
-                          input_data=input_data,
-                          fuel_data=fuel_data,
-                          spec_elec_cons=spec_elec_cons)
-
-time_series_plot(model_params['time_step'], model_params['elec_cons'])
-
-flex_case_cons = np.array(model_params['elec_cons'])
+flex_case_cons = np.array(flex_model_params['elec_cons'])
 flex_case_cost = sum(flex_case_cons*price_elec[0:optimization_horizon])
 
-cost_flexibility = (flex_case_cost - base_case_cost)/100   #Euro/MWh
+cost_flexibility = (flex_case_cost - base_case_cost)/flex_amount
 
 #%%